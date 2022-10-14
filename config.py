7cd97ae6e98dbb3564a35c8eff995a4fbe0f40ca
Daniel <daniel.rodak1@gmail.com>
2022-10-14 09:23:19 +0200

    store secrets

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
@@ -9,3 +12,17 @@ DW_DESCS_WRITE_RANGE_NAME = 'AE_S8!G2:BX48'
 COSTS_MIN_WRITE_RANGE_NAME = 'AE_Koszty!A3:H282'
 COSTS_OPT_WRITE_RANGE_NAME = 'AE_Koszty!I3:N282'
 LEADERBOARD_RANGE_NAME = 'Ranking_dup!A2:E71'
+
+if os.environ.get("PROD") == 1:
+    token_conf = {
+        "token": os.environ.get("TOKEN"),
+        "refresh_token": os.environ.get("REFESH_TOKEN"),
+        "token_uri": "https://oauth2.googleapis.com/token", 
+        "client_id": os.environ.get("CLIENT_ID"),
+        "client_secret": os.environ.get("CLIENT_SECRET"),
+        "scopes": ["https://www.googleapis.com/auth/spreadsheets"],
+        "expiry": "2022-10-14T07:16:29.384734Z"
+    }
+
+    with open('token.json', 'w') as fp:
+        json.dump(token_conf, fp)
