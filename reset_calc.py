576634ab2da89acb19a9a75076b901bd06569e47
Daniel <daniel.rodak1@gmail.com>
2023-05-11 10:52:07 +0200

    Add cron job to compute reset points for ami

--- reset_calc.py
+++ reset_calc.py
@@ -36,10 +36,16 @@ def get_reset_point(dfl):
 
 def get_report(values):
     df = pd.DataFrame(values[1:], columns=values[0])
-    dfl = df.melt(NAME_COL, value_name="Dmg").drop(columns="variable")
+    dfl = df.melt(NAME_COL, value_name="Dmg").drop(columns="variable").dropna()
     dfl[NAME_COL] = dfl[NAME_COL].str.strip()
     dfl['Dmg'] = dfl['Dmg'].astype(float)
-    display(dfl.groupby(NAME_COL).agg({'Dmg': ['mean', 'std']}))
+    dmg_frame = (
+        dfl
+        .groupby(NAME_COL)
+        .agg({'Dmg': ['mean', 'std', 'count']})
+        .assign(Has_enough_data = lambda df: df[('Dmg', 'count')] >= 50)
+    )
+    display(dmg_frame)
 
     # sns.kdeplot(x='Dmg', hue=NAME_COL, data=dfl)
 
@@ -54,69 +60,85 @@ def get_report(values):
         kde, sol = get_reset_point(dfl[dfl[NAME_COL]==player])
         res.append((player, np.round(sol.root)))
 
-    display(pd.DataFrame(res, columns=[NAME_COL, 'Reset']))
+    reset_point_frame = pd.DataFrame(res, columns=[NAME_COL, 'Reset'])
+    display(reset_point_frame)
 
     ## One reset
 
     kde, sol = get_reset_point(dfl)
     print(f"Other players reset below: {sol.root:.0f}M")
 
-    return kde, sol, dfl
-
-## Read data
+    return kde, sol, dfl, dmg_frame, reset_point_frame
 
 
-service = gs.connect()
-sheet = service.spreadsheets()
 
-ranges = {
-    "ResetCalcExample!A1:K11": "ABealinda",
-}
+def main():
 
-for r in ranges.keys():
-    print(f"##### Arkusz: {ranges[r]} #####")
-    result = sheet.values().get(spreadsheetId=SSID, range=r).execute()
-    values = result.get('values', [])
-    kde, sol, dfl = get_report(values)
+    service = gs.connect()
+    sheet = service.spreadsheets()
 
-## Simulate 
+    ranges = {
+        "Baelinda!A1:K1000": "Baelinda",
+        "Rem!A1:K1000": "Rem",
+        "ABaden_C!A1:K1000": "ABaden_C",
+        "ABrutus_D!A1:K1000": "ABrutus_D",
+        "Drez_Silas!A1:K1000": "Drez_Silas",
+        "Wathalia_Skreg!A1:K1000": "Wathalia_Skreg",
+    }
 
-    dmgs = np.linspace(1,15000,15000)
-    probs = kde(dmgs)
+    for r, s in ranges.items():
+        print(f"##### Team: {s} #####")
+        result = sheet.values().get(spreadsheetId=SSID, range=r).execute()
+        values = result.get('values', [])
+        kde, sol, dfl, dmg_frame, reset_point_frame = get_report(values)
 
-    ### No reset
-    no_reset = quad(lambda x: x*kde(x), 0, np.inf)[0]
-    no_reset_dmgs = []
-    for i in range(N_TRIES):
-        stamina = STAMINA
-        attacks = []
-        while stamina>48:
-            d = fight(dmgs, probs)
-            attacks.append(d)
-            stamina = stamina - 48
-        tot_dmg = np.sum(attacks)
-        no_reset_dmgs.append(tot_dmg)
+    ## Simulate 
 
+        dmgs = np.linspace(1,15000,15000)
+        probs = kde(dmgs)
 
-    ### Reset
-    reset_dmgs = []
-    stamina = STAMINA
-    ref_dmg = (stamina // 48) * no_reset
-    for i in range(N_TRIES):
-        stamina = STAMINA
-        attacks = []
-        while stamina>48:
-            d = fight(dmgs, probs)
-            if d < sol.root:
-                stamina = stamina - 4
-            else:
+        ### No reset
+        no_reset = quad(lambda x: x*kde(x), 0, np.inf)[0]
+        no_reset_dmgs = []
+        for i in range(N_TRIES):
+            stamina = STAMINA
+            attacks = []
+            while stamina>48:
+                d = fight(dmgs, probs)
                 attacks.append(d)
                 stamina = stamina - 48
-        mean_dmg = np.mean(attacks)
-        tot_dmg = np.sum(attacks)
-        n_attacks = len(attacks)
-        reset_dmgs.append(tot_dmg)
+            tot_dmg = np.sum(attacks)
+            no_reset_dmgs.append(tot_dmg)
 
-    gain = (np.average(reset_dmgs) - np.average(no_reset_dmgs)) / (STAMINA // 48)
-    pct_gain = gain/(np.average(no_reset_dmgs) / (STAMINA // 48))
-    print(f"Gain from the reset strategy: {gain:.0f}M ({pct_gain*100:.0f}%) per 48 stamina")
+
+        ### Reset
+        reset_dmgs = []
+        stamina = STAMINA
+        ref_dmg = (stamina // 48) * no_reset
+        for i in range(N_TRIES):
+            stamina = STAMINA
+            attacks = []
+            while stamina>48:
+                d = fight(dmgs, probs)
+                if d < sol.root:
+                    stamina = stamina - 4
+                else:
+                    attacks.append(d)
+                    stamina = stamina - 48
+            mean_dmg = np.mean(attacks)
+            tot_dmg = np.sum(attacks)
+            n_attacks = len(attacks)
+            reset_dmgs.append(tot_dmg)
+
+        gain = (np.average(reset_dmgs) - np.average(no_reset_dmgs)) / (STAMINA // 48)
+        pct_gain = gain/(np.average(no_reset_dmgs) / (STAMINA // 48))
+        print(f"Gain from the reset strategy: {gain:.0f}M ({pct_gain*100:.0f}%) per 48 stamina")
+
+        write_frame = dmg_frame.merge(reset_point_frame, on='Nick')
+        write_frame.columns = [NAME_COL, 'Mean Damage', 'Damage deviation', 'Tests count', 'Has enough data', 'Reset point']
+        write_frame = write_frame.loc[:,[NAME_COL, 'Reset point', 'Mean Damage', 'Damage deviation', 'Has enough data', 'Tests count']]
+        gs.update_values(SSID, f"{s}!M2:R{len(write_frame)+2}", "RAW", write_frame.to_numpy().tolist())
+
+
+if __name__ == '__main__':
+    main()
